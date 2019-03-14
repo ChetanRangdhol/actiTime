efaaf2abbe2970a17ff9e83739300caa3ed13b0d
Chetan <Chetan@DESKTOP-GJ6MLLF>
2019-03-14 12:25:21 +0530

    first commit

--- actiTIME/src/com/actitime/generic/AutoConstant.java
+++ actiTIME/src/com/actitime/generic/AutoConstant.java
@@ -1,5 +1,7 @@
 package com.actitime.generic;
 
+import org.testng.Reporter;
+
 public interface AutoConstant 
 {
 String chrome_key ="webdriver.chrome.driver";
@@ -7,4 +9,5 @@ String gecko_key="webdriver.gecko.driver";
 String gecko_value="./drivers/geckodriver.exe";
 String chrome_value="./drivers/chromedriver.exe";
 String File_Path="./data/input.xlsx";
+String ie_key="not present here";
 }
